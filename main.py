99697bf3b4cd2ba5d672f4f88fc8b60429825ca7
ilshat kushmanbetov <ilshat26101993@list.ru>
2025-01-31 18:27:02 +0700

    Adding class PostCreate, and processing post requests

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ class Post(BaseModel):
     author: User
 
 
+class PostCreate(BaseModel):
+    title: str
+    body: str
+    author_id: int
+
+
 users = [
     {'id': 1, 'name': 'Fin', 'age': '25'},
     {'id': 2, 'name': 'Jake', 'age': '27'},
@@ -43,6 +49,25 @@ posts = [
 @app.get('/items')
 async def items() -> List[Post]:
     return [Post(**post) for post in posts]
+
+
+@app.post('/items/add')
+async def add_item(post: PostCreate) -> Post:
+    author = next((user for user in users if user['id'] == post.author_id), None)
+    if not author:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    new_post_id = len(posts) + 1
+
+    new_post = ({
+        'id': new_post_id,
+        'title': post.title,
+        'body': post.body,
+        'author': author,
+    })
+    posts.append(new_post)
+
+    return Post(**new_post)
 
 
 @app.get('/items/{id}')
